1107a4464f7398096838e537d6c4c37d243dd774
Amida Fombutu <amida.fombutu@gmail.com>
2022-09-20 15:50:10 -0500

    more

--- Movie-project.js
+++ Movie-project.js
@@ -1,3 +1,10 @@
-fetch("https://caramel-axiomatic-class.glitch.me/movies").then(resp => resp.json() ).then(data => {
+fetch("https://caramel-axiomatic-class.glitch.me/movies").then(resp => resp.json() ).then(data =>{
+
     console.log(data)
+    const goodMovies =data.filter((movie)=>{
+        return movie.title !== ''
+    })
+    console.log(goodMovies)
+
 })
+
