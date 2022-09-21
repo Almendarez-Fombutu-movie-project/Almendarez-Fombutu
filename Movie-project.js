b0fefa74d69c96fc991b545ecee0bae9e4338c0b
Amida Fombutu <amida.fombutu@gmail.com>
2022-09-21 12:09:57 -0500

    more

--- Movie-project.js
+++ Movie-project.js
@@ -1,4 +1,6 @@
-fetch("https://caramel-axiomatic-class.glitch.me/movies").then(resp => resp.json() ).then(data =>{
+
+
+       fetch("https://caramel-axiomatic-class.glitch.me/movies").then(resp => resp.json() ).then(data =>{
 
     console.log(data)
     const goodMovies =data.filter((movie)=>{
@@ -7,7 +9,19 @@ fetch("https://caramel-axiomatic-class.glitch.me/movies").then(resp => resp.json
     console.log(goodMovies)
 let html = ''
     goodMovies.forEach((item)=>{
+html += `<div id="card1" class="card col">
+    <img src="..." class="card-img-top" alt="...">
+        <div class="card-header">Movie Title: ${item.title}</div>
+        <div class="card-body ">
+            <p class="card-text ">Director: ${item.director}</p>
+            <p class="card-text ">Genre: ${item.genre}</p>
+            <p class="card-text ">Rating: ${item.rating}</p>
+          
+        </div>
+</div>`
 
+
+document.getElementById("car-load").innerHTML=html
     })
 })
 
