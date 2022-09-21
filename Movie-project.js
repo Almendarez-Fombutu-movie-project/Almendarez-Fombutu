2fd11fcf3985fb67ee9c97f6df3b10459add15a2
Amida Fombutu <amida.fombutu@gmail.com>
2022-09-21 16:56:19 -0500

    more

--- Movie-project.js
+++ Movie-project.js
@@ -1,4 +1,16 @@
 
+var showAdd = document.querySelector('#add-btn')
+var showEdit = document.querySelector('#edit-btn')
+var showDelete = document.querySelector('#delete-btn')
+
+showAdd.addEventListener("click", function (){
+    $("#add").toggleClass(".hidden1")
+})
+
+
+
+
+
 
        fetch("https://caramel-axiomatic-class.glitch.me/movies").then(resp => resp.json() ).then(data =>{
 
@@ -8,6 +20,7 @@
     })
     console.log(goodMovies)
 let html = ''
+           let html2 = ''
     goodMovies.forEach((item)=>{
 html += `<div id="card1" class="card col">
     <img src="..." class="card-img-top" alt="...">
@@ -16,12 +29,15 @@ html += `<div id="card1" class="card col">
             <p class="card-text ">Director: ${item.director}</p>
             <p class="card-text ">Genre: ${item.genre}</p>
             <p class="card-text ">Rating: ${item.rating}</p>
+           
           
         </div>
 </div>`
+        html2 +=   `<option value=${item.id}>${item.title}</option>`
 
+document.getElementById("card-load").innerHTML=html
+        document.getElementById("selectMenu2").innerHTML=html2
 
-document.getElementById("car-load").innerHTML=html
     })
 })
 
